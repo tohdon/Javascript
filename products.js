c1369c2fa1ad8a71c6f83564bd1bb04f92c7d7ec
tohdon <74751540+tohdon@users.noreply.github.com>
2022-08-01 06:06:48 +0800

    Add files via upload

---
+++ products.js
@@ -0,0 +1,46 @@
+const products = [
+    {
+      "price": 3.99,
+      "unit": "lb",
+      "name": "Apples",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      "image": "images/products/apples.jpg",
+      "tags": [
+        "fruit",
+        "organic"
+      ]
+    },
+    
+    {
+      "price": 4.89,
+      "unit": "lb",
+      "name": "Pears",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      "image": "images/products/pear.jpg",
+      "tags": [
+        "fruit",
+        "organic"
+      ]
+    },
+    {
+      "price": 3.49,
+      "unit": "lb",
+      "name": "White Chocolate",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      "image": "images/products/white_chocolate.jpg",
+      "tags": [
+        "chocolate"
+      ]
+    },
+    {
+      "price": 0.89,
+      "unit": "each",
+      "name": "Whole Wheat Bread",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      "image": "images/products/whole_wheat_bread.jpg",
+      "tags": [
+        "bakery",
+        "whole"
+      ]
+    }
+  ];
